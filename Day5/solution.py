401ee830b73df9465dc4eb524ec346560406b132
jared-ladd <jaredladd8@gmail.com>
2024-12-05 21:53:45 -0500

    Day 5

---
+++ Day5/solution.py
@@ -0,0 +1,48 @@
+file = open("input.txt", "r")
+
+lines = []
+for line in file:
+    lines.append(line.strip())
+
+empty_line = lines.index('')
+
+rules = [line.split("|") for line in lines[:empty_line]]
+updates = [line.split(",") for line in lines[empty_line+1:]]
+
+def checkRules(update):
+    for rule in rules:
+        if rule[0] not in update or rule[1] not in update:
+            continue
+        if update.index(rule[0]) > update.index(rule[1]):
+            return False
+    return True
+
+def swapOnBrokenRules(update):
+    for rule in rules:
+        if rule[0] not in update or rule[1] not in update:
+            continue
+        left = update.index(rule[0])
+        right = update.index(rule[1])
+        if left > right:
+            tmp = update[left]
+            update[left] = update[right]
+            update[right] = tmp
+
+            
+
+# for update in updates:
+#     while not checkRules(update):
+#         swapOnBrokenRules(update)
+
+part1total = 0
+part2total = 0
+for update in updates:
+    if checkRules(update):
+        part1total += int(update[len(update) // 2])
+    else:
+        while not checkRules(update):
+            swapOnBrokenRules(update)
+        part2total += int(update[len(update) // 2])
+
+print(f'Part 1 total: {part1total}')
+print(f'Part 2 total: {part2total}')
